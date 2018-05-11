9e8b3475afcc763610297ab089c40dca997d7d75
User <user@Rabeeas-MacBook-Pro.local>
2018-05-10 21:05:23 -0400

    initial site file

---
+++ nytproject.js
@@ -0,0 +1,38 @@
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "fc7f900dfafc438d8a6b2fe05822cac4"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
+
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
+
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "fc7f900dfafc438d8a6b2fe05822cac4"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
